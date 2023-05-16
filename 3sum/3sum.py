eb5a702cbb4fe3dcc5a60e116de9116cd5a71dde
stlp <22071107+gundry2@users.noreply.github.com@users.noreply.github.com>
2023-05-15 21:03:27 -0700

    optimized 3sum two pointer method

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -30,6 +30,39 @@ class Solution:
         return list(triplets)
 
 
+        # optimized two-ptr
+        triplets = set()
+        nums = sorted(nums)
+        # go up to the 2nd-to-last element, since we need 3 for a triplet
+        for i in range(len(nums) - 1):
+            # skip duplicate values.
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+            number = nums[i]
+            ptr1 = i+1
+            ptr2 = len(nums)-1
+            while (ptr1 < ptr2):
+                curr_sum = number + nums[ptr1] + nums[ptr2]
+                if curr_sum < 0:
+                    ptr1 += 1
+                elif curr_sum > 0:
+                    ptr2 -= 1
+                else: 
+                    # break and add to triplet
+                    # do we break? hm
+                    triplets.add((number, nums[ptr1], nums[ptr2]))
+                    ptr1 += 1
+                    ptr2 -= 1
+                    # skip duplicates of other two
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+
+        print(list(triplets))
+        return list(triplets)
+
+
 if __name__ == "__main__":
     s = Solution()
     s.threeSum([1, 1, 0, -1])
