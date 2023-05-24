208e70c03248f1e95e9cc9567945d69b3924ccb3
stlp <22071107+gundry2@users.noreply.github.com@users.noreply.github.com>
2023-05-24 14:27:38 -0700

    completed 3sum in O(n^2) worst-case

--- 3sum_closest/3sum_closest.py
+++ 3sum_closest/3sum_closest.py
@@ -1,5 +1,8 @@
 # leetcode problem: https://leetcode.com/problems/3sum-closest/
 
+from typing import List
+
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         # set the closest to...something. Maybe sum of first three.
@@ -9,26 +12,35 @@ class Solution:
         sums = set()
         n = len(nums)
         nums.sort()
+        closest = float('inf')
 
         # loop to second-to-last value, since we need 3 total
+        # O(n - 2)
         for i in range(n - 2):
+            # O(1)
             slow_num = nums[i]
             left = i + 1
             right = n - 1
 
+            # O(n)...? 
+            # Really this goes from index 1 to n at worst; so n - 1 in the case there's only 3 numbers; so O(n)
             while left < right:
                 curr_sum = slow_num + nums[left] + nums[right]
                 # first, check if the current sum is smaller.
-                if curr_sum
+                if abs(target - curr_sum) < abs(target - closest): 
+                    closest = curr_sum
                 # smaller than target, so increment left
                 if curr_sum < target:
                     left += 1
                 elif curr_sum > target:
                     right -= 1
+                # if our current sum is the target just return it.
                 else:
-                    return sum
+                    return curr_sum
+
+        return closest
 
 if __name__ == "__main__":
     s = Solution()
-    s.threeSumClosest([-1,2,1,-4], 1)
-    s.threeSumClosest([0,0,0], 1)
+    print(s.threeSumClosest([-1,2,1,-4], 1))
+    print(s.threeSumClosest([0,0,0], 1))
