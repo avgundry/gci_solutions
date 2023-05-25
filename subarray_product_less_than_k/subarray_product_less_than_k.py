bf046adf323211790e0129b34db5b700d533b9be
stlp <22071107+gundry2@users.noreply.github.com@users.noreply.github.com>
2023-05-25 14:07:15 -0700

    subarr_prod_less in o(n) time

--- subarray_product_less_than_k/subarray_product_less_than_k.py
+++ subarray_product_less_than_k/subarray_product_less_than_k.py
@@ -5,41 +5,18 @@ from typing import List
 # subarray is strictly less than k.
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        # Key is the length of array; val is how many arrays of that length we've found.
-        # Will always be the maximum length for a valid array. Used to find subarrays of all arrays of
-        # length n.
-        left = right = 0
-        # number of valid arrays
-        num_arrs = 0
+        num = left = 0 
+        P = 1
+        for right in range(len(nums)):
+            P *= nums[right]
 
-        # while left in range(len(nums)):
-        #     while (curr_prod < k and right < len(nums) - 1):
-        #         right += 1
-        #         curr_prod *= nums[right]
-        #     # once we've reached the maximum length, add the arr and all subarrs to our total
-        #     num_arrs += ((right-left)**2 + (right - left))/2
-        #     # above formula is equivalent to sum from k=1 to n of k
-        #     left += 1
-        #     # Hm. is there no better way than O(n)^2???
-        
-        # I guess O(n)^2 is best for now...there *must* be improvements to this, though.
-        for left in range(len(nums)):
-            curr = 1
-            for right in range(left, len(nums)):
-                curr *= nums[right]
-                if curr < k:
-                    num_arrs += 1
-                else:
-                    break
-
-        return num_arrs
+            while (right >= left and P >= k):
+                P //= nums[left]
+                left += 1
             
+            num += right - left + 1
 
-            
-
-
-
-
+        return num            
 
 
 if __name__ == "__main__":
