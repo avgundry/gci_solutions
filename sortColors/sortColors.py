26ccd57f76764b7fb4e9b4fdf381b34b9d098f2c
stlp <22071107+gundry2@users.noreply.github.com@users.noreply.github.com>
2023-05-25 18:14:49 -0700

    linked_list_cycle - fast and slow ptr solution

--- sortColors/sortColors.py
+++ sortColors/sortColors.py
@@ -6,7 +6,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # simply add up the number then change nums accordingly??
+
+        # METHOD ONE - two passes
+        """ simply add up the number then change nums accordingly??
         zeros = 0
         ones = 0
         twos = 0
@@ -31,9 +33,31 @@ class Solution:
         # print(f"nums after ones: {nums}")
         for i in range(zeros + ones, zeros + ones + twos):
             nums[i] = 2
-        # print(f"nums after twos: {nums}")
+        # print(f"nums after twos: {nums}") """
         
-        # print(nums)
+        # METHOD TWO - one pass
+        zero_ptr = 0
+        two_ptr = len(nums) - 1
+        print(f"ORIGINAL nums: {nums}")
+        i = 0
+
+        while i <= two_ptr:
+            match nums[i]:
+                case 0:
+                    nums[i] = nums[zero_ptr]
+                    nums[zero_ptr] = 0
+                    zero_ptr += 1
+                    print(f"case zero, zero_ptr at index {zero_ptr}, nums: {nums}")
+                case 2:
+                    nums[i] = nums[two_ptr]
+                    nums[two_ptr] = 2
+                    two_ptr -= 1
+                    print(f"case 2, two_ptr at index {two_ptr}, nums: {nums}")
+                case _:
+                    print(f"case 1, nums: {nums}")
+            i += 1
+
+        print(f"\nEND nums: \n\n {nums}\n")
 
 if __name__ == "__main__":
     s = Solution()
