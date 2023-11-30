0464fdf1d8cf6d09bee39f7749f894cb851cad58
stlp <22071107+gundry2@users.noreply.github.com>
2023-11-30 09:46:28 -0800

    sliding window median - missing only one case due to time constraints

--- sliding_window_median.py
+++ sliding_window_median.py
@@ -3,33 +3,78 @@ from typing import List
 
 
 class Solution:
+    def __init__(self):
+        # contains largest half of nums in window
+        self.min_heap = []
+        # contains smallest half of nums in window
+        self.max_heap = []
+
+    def rebalance(self):
+        # rebalance
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        elif len(self.min_heap) > len(self.max_heap):
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+
+
+    def addNum(self, num):
+        if len(self.max_heap) == 0 or -num > self.max_heap[0]:
+            heapq.heappush(self.max_heap, -num)
+        # nums[i] is smaller than the largest number in self.max_heap, so is in
+        # smallest half of numbers
+        else:
+            heapq.heappush(self.min_heap, num)
+
+        self.rebalance()
+
+    def median(self):
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        else:
+            return (self.min_heap[0] - self.max_heap[0]) / 2
+
+
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         medians = []
         if k > len(nums) or nums == None or nums == []:
             return []
 
-        # contains largest half of nums in window
-        min_heap = []
-        # contains smallest half of nums in window
-        max_heap = []
+        # to begin, fill the heaps with the first k numbers
+        for i in range(k):
+            self.addNum(nums[i])
 
+        # add the first median
+        medians.append(self.median())
+
+        # then slide the window. 
+        for i in range(k + 1, len(nums) + 1):
+            # for testing purposes, add the window over again each time
+            # obv horrible runtime
+            self.min_heap.clear()
+            self.max_heap.clear()
+            for j in range(i - k, i):
+                self.addNum(nums[j])
+            medians.append(self.median())
+            # # remove the number at i from the heap...hm.
+            # # is this O(n)? yes...so this will be O(n^2). Hm
+            # if -nums[i - k] in self.max_heap:
+            #     self.max_heap.remove(-nums[i - k])
+            # else:
+            #     self.min_heap.remove(nums[i - k])
+            # self.rebalance()
+
+            # # then add number at right side of window
+            # self.addNum(nums[i])
+
+            # # and finally, add the median of this window to the arr
+            # medians.append(self.median())
         
 
-        # Brute force naive solution - simply sort window every time
-        # O(kn^2) time, O(k) space
-        # Appears to work but FAILS on large inputs due to egregious runtime
-        # for i in range(len(nums) - k + 1):
-        #     window = nums[i:i+k]
-        #     if k % 2 == 0:
-        #         # if even we have to divide middle two
-        #         medians.append(sorted(window)[len(window) // 2]/2 + window[len(window) // 2 + 1]/2)
-        #     else:
-        #         # otherwise just append middle
-        #         medians.append(sorted(window)[len(window) // 2])
-
-        # return medians
+        print(self.max_heap, self.min_heap)
+        print(f"current median: {self.median()}")
+        return medians
         
 if __name__ == "__main__":
     s = Solution()
     nums = [1,3,-1,-3,5,3,6,7]
-    print(s.medianSlidingWindow(nums, 3))
+    print(f"medians for {nums} with window size {3}:\n {s.medianSlidingWindow(nums, 3)}")
