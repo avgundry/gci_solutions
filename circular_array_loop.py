81962286c39ab56757423c10631a745db3ddf349
stlp <22071107+gundry2@users.noreply.github.com>
2023-12-19 12:54:07 -0800

    rotate_list.py in O(n) time, O(1) space

--- circular_array_loop.py
+++ circular_array_loop.py
@@ -4,31 +4,40 @@ from typing import List
 class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
         n = len(nums)
-        fast = slow = 0
-        fast = (fast + nums[fast]) % n
-        first_neg = nums[0] < 0
-        same_sign = True
-        # oh my god
-        while fast != slow:
-            if nums[slow] < 0 != first_neg:
-                same_sign = False
-            slow = (slow + nums[slow]) % n
-            if nums[fast] < 0 != first_neg:
-                same_sign = False
-            fast = (fast + nums[fast]) % n
-            if nums[fast] < 0 != first_neg:
-                same_sign = False
-            fast = (fast + nums[fast]) % n
-
-        if fast != 0:
-            # cycle of len 1
-            if (fast + nums[fast]) % n == fast or not same_sign:
-                return False
-            return True
-        else:
+        if n == 0:
             return False
+
+        cycles = [-1 for _ in range(n)]
+        for pos in range(n):
+            curr = pos
+            # determine cycle
+            cyc = set()
+            while curr not in cyc:
+                cyc.add(curr)
+                move = nums[curr] % n
+                if move + curr >= n:
+                    curr = move + curr - n
+                else:
+                    curr += move
+            cycles[pos] = cyc
+
+        all_same = True
+        for c in cycles:
+            prev = None
+            for num in c:
+                if prev:
+                    if prev < 0 != num < 0:
+                        break
+
+                prev = num
+                
+            if prev < 0 == num < 0:
+                return True
+
+        return False
+
+
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.circularArrayLoop([-2,1,-1,-2,-2]))     # should be false.
-
+    print(s.circularArrayLoop([2, -1, 1, 2, 2]))
--- rotate_list.py
+++ rotate_list.py
@@ -9,7 +9,6 @@ class ListNode:
 
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # first find the node where we're reattach
         if k == 0 or head == None or head.next == None:
             return head
 
@@ -27,33 +26,44 @@ class Solution:
 
         # find length of LL first, to avoid needlessly looping.
         n = 1
-        while curr != None:
+        while curr.next != None:
             curr = curr.next
             n += 1
+
+        # print(n) 
+        # if we don't rotate, simply return
+        if (k % n) == 0:
+            return head
+
+        # after looping through we will have found end of the old list.
+        # we need this to attach it to the new head. 
+        old_end = curr
+
+        # then. find the new head, and the new end of the list.
+        # modulo k by n to avoid needlessly looping
         k %= n
-        while k > 0:
-            prev = curr
-            # end of list.
-            # if curr.next == None:
-            #     curr = head
-            # else:
+        # then advance by (n - k) to find new head and new end of list
+        new_end = None
+        curr = head
+        for i in range(n - k):
+            new_end = curr
             curr = curr.next
-            k -= 1
 
-        end = curr
-        while end.next != None:
-            end = end.next
+        # print(f"new head: {curr}")
+        # print(f"new end: {new_end}")
+        # print(f"old head: {head}")
+        # print(f"old end: {old_end}")
 
-        print(f"curr: {curr}")
-        print(f"prev: {prev}")
-        print(f"end: {end}")
-
-        prev.next = None
-        end.next = head
+        # then just link them all up.
+        # old end -> old head.
+        # new end -> None.
+        # head = new_head.
+        old_end.next = head
+        new_end.next = None
         head = curr
 
-        return head
+        return head 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.rotateRight([1, 2, 3], 1))
+    # print(s.rotateRight([1, 2, 3], 1))
