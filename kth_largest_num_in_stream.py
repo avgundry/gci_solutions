2c249c4ba43ed11e579b44e0940bc25df38ba598
stlp <22071107+gundry2@users.noreply.github.com>
2024-01-02 09:34:40 -0800

    Poor solution to kth_largest_num_in_stream

--- kth_largest_num_in_stream.py
+++ kth_largest_num_in_stream.py
@@ -1,38 +1,65 @@
-# seeing if it's diff in Python
-import collections
-import heapq
+from heapq import *
 from typing import List
 
-
 class KthLargest:
-
+    """
+    Two-heap method.
+    """
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.minHeap = []
-        for num in nums:
-            self.add(num)
-        # I'm so incredibly stupid. god I've wasted like 30 mins on this
+        self.nums = []
+        self.klargest = nums
+        heapify(self.klargest)
+        while len(self.klargest) > k:
+            self.nums.append(heappop(self.klargest))
 
     def add(self, val: int) -> int:
-        if len(self.minHeap) > self.k - 1:
-            heapq.heappushpop(self.minHeap, val)
+        if len(self.klargest) < self.k:
+            heappush(self.klargest, val)
+        elif val > self.klargest[0]:
+            self.nums.append(heappushpop(self.klargest, val))
         else:
-            heapq.heappush(self.minHeap, val)
+            self.nums.append(val)
+            heappush(self.nums, val)
 
-        return self.minHeap[0]
+        return self.klargest[0]
 
+
+    """
+    Brute force method. Times out for large inputs.
+    """
+    # def __init__(self, k: int, nums: List[int]):
+    #     self.k: int = k
+    #     # This really feels like a two-heap problem, honestly. Hm.
+    #     # Or at least it could be sped up a lot by doing so...anyways.
+    #     # Trying with one heap first.
+
+    #     self.nums: List[int] = nums
+    #     # Takes O(len(nums)) time.
+    #     heapify(self.nums)
+        
+
+    # def add(self, val: int) -> int:
+    #     # Brute force method is to add to the heap, then use heapq's
+    #     # nlargest to get the kth largest element.
+    #     heappush(self.nums, val)
+        
+    #     if self.k > 100:
+    #         return sorted(self.nums)[-self.k]
+    #     else:
+    #         return nlargest(self.k, self.nums)[-1]
+
+if __name__ == "__main__":
+    s = KthLargest(3, [4, 5, 8, 2])
+    print(s.add(3))
+    print(s.add(5))
+    print(s.add(10))
+    print(s.add(9))
+    print(s.add(4))
+
+        
 
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
-
-if __name__ == "__main__":
-    obj = KthLargest(3, [4,5,8,2])
-    param_1 = obj.add(3)
-    param_2 = obj.add(5)
-    param_3 = obj.add(10)
-    param_4 = obj.add(9)
-    param_5 = obj.add(4)
-    print(param_1)
-    print(param_2)
